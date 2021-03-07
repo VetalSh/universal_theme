04c5e6e76fe553fd3aee0c2312841b6f1dad091c
VetalSh <jedvemya86@gmail.com>
2021-03-07 22:35:35 +0200

    Add posts on front page

--- functions.php
+++ functions.php
@@ -5,6 +5,9 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
 		// Добавление тега title
 		add_theme_support( 'title-tag' );
 
+		// Добавление миниатюр
+		add_theme_support( 'post-thumbnails', array( 'post' ) );
+
 		// Добавление пользовательского логотипа
 		add_theme_support( 'custom-logo', [
 			'width'       => 163,
@@ -26,5 +29,6 @@ add_action( 'after_setup_theme', 'universal_theme_setup' );
 function enqueue_universal_style() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style');
+	wp_enqueue_style( 'Roboto-Slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
