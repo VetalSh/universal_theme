326580ec95c9b77bd1fb11a5bad6a71ff472b301
VetalSh <jedvemya86@gmail.com>
2021-03-14 13:14:14 +0200

    Add investigation and digest

--- front-page.php
+++ front-page.php
@@ -127,6 +127,7 @@
       // Формируем запрос в базу данных
       $query = new WP_Query( [
         'posts_per_page' => 7,
+        'category__not_in' => 23,
       ] );
   
       // Проверяем есть ли посты?
@@ -209,7 +210,7 @@
                 <li class="article-grid-item article-grid-item-3">
                   <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
                     <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="article-thumb">
-                    <h4 class="article-grid-title"><?php echo the_title(); ?></h4>
+                    <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?></h4>
                   </a>
                 </li>
               <?php
@@ -246,3 +247,98 @@
   <!-- /.main-grid -->
 </div>
 <!-- /.container -->
+
+<?php		
+global $post;
+
+$query = new WP_Query( [
+	'posts_per_page' => 1,
+	'category_name'  => 'investigation',
+] );
+
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		?>
+		<section class="investigation" style="background: linear-gradient(0deg, rgba(64, 48, 61, 0.45), rgba(64, 48, 61, 0.45)), url(<?php echo get_the_post_thumbnail_url(); ?>) no-repeat center center">
+      <div class="container">
+        <h2 class="investigation-title"><?php the_title(); ?></h2>
+        <a href="<?php echo get_the_permalink(); ?>" class="more">Читать статью</a>
+      </div>
+    </section>
+    <!-- /.investigation -->
+		<?php 
+	}
+} else {
+	// Постов не найдено
+}
+
+wp_reset_postdata(); // Сбрасываем $post
+?>
+
+<!-- вывести на главной странице список статей, которые идут дальше по макету -->
+<div class="container">
+  <div class="digest-wrapper">
+    <ul class="digest">
+      <?php
+      global $post;
+  
+      $myposts = get_posts([ 
+        'numberposts' => 6,
+        'category_name' => 'news, opinion, articles',
+        'category__not_in' => 23,
+      ]);
+  
+      if( $myposts ){
+        foreach( $myposts as $post ){
+          setup_postdata( $post );
+          ?>
+          <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+          <li class="digest-item">
+            <a href="<?php echo get_the_permalink(); ?>" class="digest-item-permalink">
+              <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="digest-thumb">
+            </a>
+            <div class="digest-info">
+              <button class="bookmark">
+                <svg width="14" height="18" class="icon icon-bookmark">
+                  <use xlink:href=""></use>
+                </svg>
+              </button>
+              <a href="#" class="category-link javascript"><?php $category = get_the_category(); echo $category[0]->name; ?></a>
+              <a href="#" class="digest-item-permalink">
+                <h3 class="digest-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?></h3>
+              </a>
+              <p class="digest-excerpt"><?php echo mb_strimwidth(get_the_excerpt(), 0, 160, '...'); ?></p>
+              <div class="digest-footer">
+                <span class="digest-date"><?php the_time('j F'); ?></span>
+                <div class="comments digest-comments">
+                  <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/comment.svg'; ?>" class="icon comments-icon" alt="icon: comment">
+                  <span class="comments-counter"><?php comments_number( "0", '1', '%' ); ?></span>
+                </div>
+                <div class="likes digest-likes">
+                  <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/heart.svg'; ?>" alt="icon: like" class="likes-icon">
+                  <span class="likes-counter"><?php comments_number( "0", '1', '%' ); ?></span>
+                </div>
+              </div>
+              <!-- /.digest-footer -->
+            </div>
+            <!-- /.digest-info -->
+          </li>
+          <?php 
+        }
+      } else {
+        // Постов не найдено
+        ?> <p>Постов нет</p> <?php
+      }
+  
+      wp_reset_postdata(); // Сбрасываем $post
+      ?>
+    </ul>
+  </div>
+  <!-- /.digest-wrapper -->
+</div>
+<!-- /.container -->
+
+
+
+
